dfae4bb0cfee285f47fadf8b2dc0175f7599f2a9
Duo <shatz10@outlook.com>
2024-12-03 23:54:51 +0800

    Wait for Gameplay Event
    Play Montage and Wait

--- Source/Warrior/Private/WarrioGameplayTags.cpp
+++ Source/Warrior/Private/WarrioGameplayTags.cpp
@@ -8,4 +8,8 @@ namespace WarriorGameplayTags
 	UE_DEFINE_GAMEPLAY_TAG(Input_Ability_UnequipAxe, "Input.Ability.UnequipAxe")
 	
 	UE_DEFINE_GAMEPLAY_TAG(PlayerTag_Weapon_Axe, "Player.Weapon.Axe")
+
+	// Event Tags
+	UE_DEFINE_GAMEPLAY_TAG(Player_Event_Equip_Axe, "Player.Event.Equip.Axe")
+	UE_DEFINE_GAMEPLAY_TAG(Player_Event_Unequip_Axe, "Player.Event.Unequip.Axe")
 }
